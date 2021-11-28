bc140983d1d897f8e127d48e69b19ae0e73fe15b
Rusmansyah Putra NH <rusman.putra.712@gmail.com>
2021-11-28 20:54:32 +0700

    fixing search data array by array keyword

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -1,105 +1,107 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Search extends CI_Controller {
+class Search extends CI_Controller
+{
 
     //Konstruktor Untuk Controller 
-	public function __construct()
+    public function __construct()
     {
-		parent::__construct();
+        parent::__construct();
         //Memuat Model Search
         $this->load->model('Search_model', 'search'); //load search_model as search
     }
-    
+
     public function index()
     {
         // ambil data keyword
-        if($this->input->post('submit')){
+        if ($this->input->post('submit')) {
             $data['keyword'] = $this->input->post('keyword');
-            
-            if ($data['keyword'] == ''){
+
+            if ($data['keyword'] == '') {
                 // jika keyword kosong
                 $this->session->set_userdata('alert', '1');
                 redirect(base_url());
             }
-            
+
             $this->session->unset_userdata('alert');
-        }else {
+        } else {
             $data['keyword'] = $this->session->userdata('keyword');
         }
-        
+
         $data['arrKeyword'] = $this->olahKeyword($data['keyword']);
-        $data['lowongan'] = $this->search->getLowongan($data['arrKeyword']);   
+        $data['lowongan'] = $this->search->getLowongan($data['arrKeyword']);
         $data['total_rows'] = $this->search->countAllRows($data['arrKeyword']);
 
         // jika kata kunci tidak ditemukan
-        if (empty($data['lowongan'])){
+        if (empty($data['lowongan'])) {
             $this->session->set_userdata('alert', '2');
             $this->session->set_userdata('keyword', $data['keyword']);
             redirect(base_url());
-        }else{
+        } else {
             $this->session->unset_userdata('alert');
-            
+
             // hitung nilai TF
             $data['nilai_tf'] = $this->fungsiHitungTF($data['lowongan'], $data['arrKeyword']);
             // hitung nilai IDF
             $data['nilai_idf'] = $this->fungsiHitungIDF($data['nilai_tf'], $data['arrKeyword'], $data['total_rows']);
             // hitung nilai tf*idf
             $data['nilai_tfidf'] = $this->fungsiHitungTFIDF($data['nilai_tf'], $data['nilai_idf'], $data['arrKeyword']);
-    
+
             $this->load->view('template/Header', $data);
             $this->load->view('search/Index', $data);
             $this->load->view('template/Footer');
         }
-
     }
 
-    public function olahKeyword($Keyword = null){
+    public function olahKeyword($Keyword = null)
+    {
         $rawKeyword = explode(' ', $Keyword);
-        
+
         // stem keyword
         $arrKeyword = array();
-        foreach ($rawKeyword as $x){     
+        foreach ($rawKeyword as $x) {
             // create nadar stemmer
             $stemmed = \Nadar\Stemming\Stemm::stem($x, 'en');
-            
+
             // create sastrawi stemmer
             $stemmerFactory = new \Sastrawi\Stemmer\StemmerFactory();
             $stemmer  = $stemmerFactory->createStemmer();
             // stem
             $output   = $stemmer->stem($stemmed);
-            
+
             // fungsi mengatasi kata yang sama pada keyword (double)
-            if(array_search($output, $arrKeyword) === false){
+            if (array_search($output, $arrKeyword) === false) {
                 array_push($arrKeyword, trim($output));
             }
-            
+
             // mengecek keyword dan menambahkan jika null
             $this->search->cekKeyword($arrKeyword);
         }
-        
+
         return $arrKeyword;
     }
 
-    public function fungsiHitungTF($dataLowongan, $arrKeyword){
+    public function fungsiHitungTF($dataLowongan, $arrKeyword)
+    {
         // hitung nilai TF
         $nilai_tf = array();
-        foreach ($dataLowongan as $colLow){
+        foreach ($dataLowongan as $colLow) {
             // untuk setiap stem_detail
             $arrKalimat = explode(' ', $colLow['stem_detail']);
 
             $counter = array();
-            
+
             $counter['id_tf'] = $colLow['id_lowongan'];
             $counter['title_tf'] = $colLow['title_lowongan'];
             $counter['link_tf'] = $colLow['link_lowongan'];
-            
-            $allTf = 0; 
-            foreach($arrKeyword as $key){
-                $tf = 0; 
-                foreach($arrKalimat as $kal){
+
+            $allTf = 0;
+            foreach ($arrKeyword as $key) {
+                $tf = 0;
+                foreach ($arrKalimat as $kal) {
                     // jika keyword sama dengan kata pada stem_detail
-                    if($key == $kal){
+                    if ($key == $kal) {
                         $tf++;
                         $allTf++;
                     }
@@ -110,7 +112,7 @@ class Search extends CI_Controller {
 
             // jika nilainya nol maka dihilangkan dari list
             // if($allTf != 0){
-                array_push($nilai_tf, $counter);
+            array_push($nilai_tf, $counter);
             // }
         }
 
@@ -121,53 +123,55 @@ class Search extends CI_Controller {
         return $nilai_tf;
     }
 
-    public function fungsiHitungIDF($nilai_tf, $arrKeyword, $dataTotalRows){
+    public function fungsiHitungIDF($nilai_tf, $arrKeyword, $dataTotalRows)
+    {
         $nilai_idf = array();
-        foreach($arrKeyword as $key){
+        foreach ($arrKeyword as $key) {
             $counter = 0;
-            foreach($nilai_tf as $tf){
-                if($tf[$key] != 0)
-                $counter++;
+            foreach ($nilai_tf as $tf) {
+                if ($tf[$key] != 0)
+                    $counter++;
             }
             $df['term'] = $key;
             $df['df'] = $counter;
             if (!$counter == 0) {
                 try {
-                    $df['idf'] = log($dataTotalRows/$counter);
-                }catch(DivisionByZeroError $e){
+                    $df['idf'] = log($dataTotalRows / $counter);
+                } catch (DivisionByZeroError $e) {
                     $df['idf'] = $e;
                 }
-            }else{
+            } else {
                 $df['idf'] = $counter;
             }
-            
+
             array_push($nilai_idf, $df);
         }
 
         return $nilai_idf;
     }
 
-    public function fungsiHitungTFIDF($nilai_tf, $nilai_idf, $arrKeyword){
+    public function fungsiHitungTFIDF($nilai_tf, $nilai_idf, $arrKeyword)
+    {
         $nilai_tfidf = array();
-        foreach($nilai_tf as $tf){
+        foreach ($nilai_tf as $tf) {
             $counter = array();
-            
+
             $counter['id_tfidf'] = $tf['id_tf'];
             $counter['title_tfidf'] = $tf['title_tf'];
             $counter['link_tfidf'] = $tf['link_tf'];
-            
+
             $allTfidf = 0;
-            foreach($arrKeyword as $key){
-                $tfidf = 0; 
-                foreach($nilai_idf as $idf){
+            foreach ($arrKeyword as $key) {
+                $tfidf = 0;
+                foreach ($nilai_idf as $idf) {
                     // jika ada kata kunci null
-                    if($idf['term'] == $key){
-                        if(!empty($idf['idf'])){
+                    if ($idf['term'] == $key) {
+                        if (!empty($idf['idf'])) {
                             $tfidf = $idf['idf'] * $tf[$key];
-                        }else{
+                        } else {
                             $tfidf = 0;
                         }
-                        
+
                         $allTfidf = $allTfidf + $tfidf;
                     }
                 }
@@ -184,5 +188,4 @@ class Search extends CI_Controller {
 
         return $nilai_tfidf;
     }
-
 }
--- application/models/Search_model.php
+++ application/models/Search_model.php
@@ -1,37 +1,34 @@
-<?php 
+<?php
 
-class Search_model extends CI_Model{
+class Search_model extends CI_Model
+{
 
-    public function getLowongan($arrKeyword){
-        foreach ($arrKeyword as $arrKey){
-            $this->db->like('title_lowongan', $arrKey);
-            $this->db->or_like('lokasi_perusahaan', $arrKey);
+    public function getLowongan($arrKeyword)
+    {
+        foreach ($arrKeyword as $arrKey) {
             $this->db->or_like('stem_detail', $arrKey);
-            $this->db->or_like('nama_perusahaan', $arrKey);
         }
 
         return $this->db->get('lowongan')->result_array();
     }
 
-    public function countAllRows($arrKeyword){
-        foreach ($arrKeyword as $arrKey){
-            $this->db->like('title_lowongan', $arrKey);
-            $this->db->or_like('lokasi_perusahaan', $arrKey);
+    public function countAllRows($arrKeyword)
+    {
+        foreach ($arrKeyword as $arrKey) {
             $this->db->or_like('stem_detail', $arrKey);
-            $this->db->or_like('nama_perusahaan', $arrKey);
         }
 
-        return $this->db->from('lowongan')->count_all_results();//menghitung jumlah barisan pada query terakhir
+        return $this->db->from('lowongan')->count_all_results(); //menghitung jumlah barisan pada query terakhir
     }
 
-    public function cekKeyword($Keyword){
-        foreach($Keyword as $key){
+    public function cekKeyword($Keyword)
+    {
+        foreach ($Keyword as $key) {
             $dataKeyword = $this->db->get_where('keyword', array('word' => $key))->num_rows();
 
-            if($dataKeyword == 0){
+            if ($dataKeyword == 0) {
                 $this->db->insert('keyword', array('word' => $key));
             }
         }
     }
-
 }
